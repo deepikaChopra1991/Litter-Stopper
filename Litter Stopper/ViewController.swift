fe6a59a2bd49aed681075b3618c1347fc3c5033f
Sunfocus Solutions <development.sunfocus@gmail.com>
2018-08-13 16:09:46 +0530

    Initial Commit

---
+++ Litter Stopper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Litter Stopper
+//
+//  Created by Applr on 13/08/18.
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
